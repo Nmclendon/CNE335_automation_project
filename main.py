1cab049d8f8d3fad17bbc36d134b10553c85c3c2
Nmclendon <nicholas.a.mclendon@gmail.com>
2023-12-05 09:14:00 -0800

    Added automated Ubuntu update.
    Added code that connects to the EC2 via SSH and upgrades it's version of Ubuntu.

--- main.py
+++ main.py
@@ -2,17 +2,28 @@
 # CNE 335 Fall 2023
 # Automate ping/SSH
 from Server import Server
+import paramiko
 
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Nicholas McLendon")
+    print("Server Automator v0.1 by Nicholas McLendon"
 
 # This is the entry point to our program
 if __name__ == '__main__':
+    ssh = paramiko.SSHClient()
+    key = paramiko.RSAKey.from_private_key_file(r'C:\Users\Owner\Documents\Schoolwork\CNE335\AWSproject\CNE335-NicholasMcLendon-F23-NicholasMcLendon-EC2forFinal.pem')
+    ec2Server = Server('34.210.125.71')
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname='34.210.125.71', username='ubuntu', pkey=key)
+
     print_program_info()
 
-    # TODO - Create a Server object
-    ec2Server = Server('34.217.207.38')
+    stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo-apt upgrade -y && sudo-apt autoremove && sudo apt autoclean')
+    line = stdout.readline()
 
-    # TODO - Call Ping method and print the results
+    while line:
+        line = stdout.readline()
+        print(line)
+
+    ssh.close()
+
     request = ec2Server.ping()
